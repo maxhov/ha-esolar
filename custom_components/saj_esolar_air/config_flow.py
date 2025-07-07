7d18fd427e748a580e93f6c82641b2787619e3d9
maxhov <14804474+maxhov@users.noreply.github.com>
2025-07-07 14:17:33 +0200

    Fix deprecation

--- custom_components/saj_esolar_air/config_flow.py
+++ custom_components/saj_esolar_air/config_flow.py
@@ -159,17 +159,13 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @callback
     def async_get_options_flow(
         config_entry: config_entries.ConfigEntry,
-    ) -> config_entries.OptionsFlow:
+    ) -> OptionsFlowHandler:
         """Create the options flow."""
-        return OptionsFlowHandler(config_entry)
+        return OptionsFlowHandler()
 
 
 class OptionsFlowHandler(config_entries.OptionsFlow):
     """Handle a options flow for eSolar."""
-
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
